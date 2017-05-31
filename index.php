6fcc6596da3e092e28b0efdf011e679e4bf5092c
synefo <synefo@users.noreply.github.com>
2017-05-31 12:30:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,83 @@
 <?php 
-echo 'oo';
+header('Content-type: application/json');
+$folderName = uniqid();
+$zipFileName = $folderName . '.zip';
+
+if(!isset($_POST['zipContent'])){
+	http_response_code(400);
+	return;
+}
+
+$zipStr = $_POST['zipContent'];
+
+if(!$zipStr){
+	http_response_code(400);
+	return;
+}
+
+
+$basedata =  base64_decode($zipStr);
+$f = fopen ($zipFileName, "a+");
+fwrite($f, $basedata);
+fclose($f);
+
+$zip = new ZipArchive(); 
+  
+if ($zip->open($zipFileName, ZipArchive::CREATE)!== TRUE) {
+	exit("Impossible d'ouvrir le fichier <$zipFileName>\n");
+}
+
+   
+$zip->extractTo($folderName);
+
+$zip->close();
+
+
+$zipContent = new FileItem($folderName);
+
+
+
+echo json_encode($zipContent);
+
+unlink($zipFileName);
+
+
+
+
+class FileItem{
+	
+	public $type;
+	public $name;
+	public $children;
+	public $data;
+	
+	public function FileItem($extractPath){
+		
+	
+		if(gettype($extractPath) == 'string'){
+			$this->name = $extractPath;
+			$this->type = 'dir';
+			$this->children = [];
+			$files = new FilesystemIterator($extractPath);
+			
+			foreach ($files as $fileInfo){
+				if($fileInfo->isDir())
+					array_push($this->children, new FileItem($extractPath . '/' . $fileInfo->getFilename()));
+				else
+					array_push($this->children, new FileItem($fileInfo));
+			}
+			rmdir($extractPath);
+		}
+		else{
+			$this->type = 'file';
+			$this->name = $extractPath->getFilename();
+			$this->data = base64_encode(file_get_contents($extractPath->getRealpath()));
+			unlink($extractPath->getRealpath());
+		}
+			
+		
+	}
+}
+
+
 ?>
