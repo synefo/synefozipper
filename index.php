df4a513e4898c55b386643f6d949cdb1ee29bb9b
synefo <synefo@users.noreply.github.com>
2017-12-28 17:29:04 +0100

    Update index.php

--- index.php
+++ index.php
@@ -79,7 +79,8 @@ class FileItem{
 		$this->name = $file->getFilename();
 		$this->data = base64_encode(file_get_contents($file->getRealpath()));
 		$this->folder = str_replace('\\', '', str_replace($baseFolder, '', $file->getPath()));
-		$this->folder2 = str_replace('\', '', str_replace($baseFolder, '', $file->getPath()));
+		$this->folder2 = str_replace('\\\', '', str_replace($baseFolder, '', $file->getPath()));
+		
 		//$this->folder3 = $file->getPathname();
 		$this->extension = $file->getExtension();
 		unlink($file->getRealpath());
