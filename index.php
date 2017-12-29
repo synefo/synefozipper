c783f22db621464105ac7bc19e015f6a78755690
synefo <synefo@users.noreply.github.com>
2017-12-29 17:31:56 +0100

    Update index.php

--- index.php
+++ index.php
@@ -56,7 +56,7 @@ echo json_encode($zipContent);
 
 unlink($zipFileName);
 
-public static function includeFile($ignoreMetaXml, $fileName){
+function includeFile($ignoreMetaXml, $fileName){
 	return !$ignoreMetaXml || ($fileName != 'package.xml' && !strstr($fileName, '-meta.xml'));
 }
 
